9077b62479887c7c3a206bada80c2a3f0ca3b70f
Johan Faerch <jf@keeptrack.nu>
2018-05-01 11:19:24 +0200

    News from Reuter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @newsapi = News.new(ENV["newsapi_api_key"])
+    @headlines = @newsapi.get_top_headlines(sources: "reuters")
   end
 end
