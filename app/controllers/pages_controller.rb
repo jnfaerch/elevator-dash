9366dafc177e44433092df25af6944707b36d652
Johan Faerch <jf@keeptrack.nu>
2018-05-04 15:18:06 +0200

    Add listener for bus api up/down and reload on 'old' departure time

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   require 'news-api'
   require 'open-uri'
+  require 'uri'
+  require 'net/http'
 
   skip_before_action :authenticate_user!, only: [:home]
 
@@ -8,34 +10,67 @@ class PagesController < ApplicationController
     @newsapi = News.new(ENV['newsapi_api_key'])
     @headlines = @newsapi.get_top_headlines(country: 'se')
 
-    @busjson = JSON.load(busjson).to_hash
-    @busno = @busjson.first[1].first["Product"]["num"] # The bus No
+    @busjson = busjson
+    @busno = busno # The bus No
     @bustime = bustime # The next bus planned or real departure time - ie 19:46
-    @bustime2 = bustime2 # The next bus after that
+    @bustime_hours = bustime_hours
+    @bustime_minutes = bustime_minutes
+    @bustime2 = bustime2 # The next departure after bustime
     @timenow = Time.now
     @hournow = Time.now.hour
-    @minnow = Time.now.to_s.split[1][3..4].to_i
+    @minnow = Time.now.to_s.split[1][3..4]
   end
 
   private
 
+  def online # Checks if resrobot api link is up!
+    # uri = URI::HTTPS.build([nil, "api.resrobot.se", nil, "/v2/departureBoard", "key=#{ENV["resrobot_api_key"]}&id=740015647&direction=740000003&maxJourneys=4&products=128&format=json", nil])
+    uri = URI.parse("https://api.resrobot.se/v2/departureBoard?key=#{ENV['resrobot_api_key']}&id=740015647&direction=740000003&maxJourneys=4&products=128&format=json")
+    Net::HTTP.get_response(uri).code == "200"
+  end
+
   def busjson
-    open("https://api.resrobot.se/v2/departureBoard?key=#{ENV['resrobot_api_key']}&id=740015647&direction=740000003&maxJourneys=4&products=128&format=json").read
+    if online
+      bus = open("https://api.resrobot.se/v2/departureBoard?key=#{ENV['resrobot_api_key']}&id=740015647&direction=740000003&maxJourneys=4&products=128&format=json").read
+        JSON.load(bus).to_hash
+    end
+  end
+
+  def busno
+    if online
+      @busjson.first[1].first["Product"]["num"]
+    end
   end
 
   def bustime
-    if @busjson.first[1][0]["Stops"]["Stop"][0]["rtDepTime"] == nil
-      @busjson.first[1][0]["Stops"]["Stop"][0]["depTime"][0..4]
-    else
-      @busjson.first[1][0]["Stops"]["Stop"][0]["rtDepTime"][0..4]
+    if online
+      if @busjson.first[1][0]["Stops"]["Stop"][0]["rtDepTime"] == nil
+        @busjson.first[1][0]["Stops"]["Stop"][0]["depTime"][0..4]
+      else
+        @busjson.first[1][0]["Stops"]["Stop"][0]["rtDepTime"][0..4]
+      end
+    end
+  end
+
+  def bustime_hours
+    if online
+      bustime[0..1]
+    end
+  end
+
+  def bustime_minutes
+    if online
+      bustime[3..4]
     end
   end
 
   def bustime2
-    if @busjson.first[1][1]["Stops"]["Stop"][0]["rtDepTime"] == nil
-      @busjson.first[1][1]["Stops"]["Stop"][0]["depTime"][0..4]
-    else
-      @busjson.first[1][1]["Stops"]["Stop"][0]["rtDepTime"][0..4]
+    if online
+      if @busjson.first[1][1]["Stops"]["Stop"][0]["rtDepTime"] == nil
+        @busjson.first[1][1]["Stops"]["Stop"][0]["depTime"][0..4]
+      else
+        @busjson.first[1][1]["Stops"]["Stop"][0]["rtDepTime"][0..4]
+      end
     end
   end
 
